1f684bf17179e5b2180b6ded5084c3e6897f31e8
CARLOSLUISGUTIERREZRAMOS <carlos5t@hotmail.com>
2019-04-29 21:05:56 -0500

    Update database.php

--- application/config/database.php
+++ application/config/database.php
@@ -1,27 +1,31 @@
 <?php
+
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 $active_group = 'default';
 $query_builder = TRUE;
 
 $db['default'] = array(
-	'dsn'	=> '',
-	'hostname' => '127.0.0.1',
-	'username' => 'user_db_siv',
-	'password' => 's1v515',
-	'database' => 'db_siv',
-	'dbdriver' => 'mysqli',
-	'dbprefix' => '',
-	'pconnect' => FALSE,
-	'db_debug' => (ENVIRONMENT !== 'production'),
-	'cache_on' => FALSE,
-	'cachedir' => '',
-	'char_set' => 'utf8',
-	'dbcollat' => 'utf8_general_ci',
-	'swap_pre' => '',
-	'encrypt' => FALSE,
-	'compress' => FALSE,
-	'stricton' => FALSE,
-	'failover' => array(),
-	'save_queries' => TRUE
+    'dsn' => '',
+//    'hostname' => '104.198.247.7',
+//    'username' => 'root',
+//    'password' => 'DK7C54HCkQ4GXWh',
+    'hostname' => '127.0.0.1',
+    'username' => 'root',
+    'password' => '',
+    'database' => 'db_siv',
+    'dbdriver' => 'mysqli',
+    'dbprefix' => '',
+    'pconnect' => FALSE,
+    'db_debug' => (ENVIRONMENT !== 'production'),
+    'cache_on' => FALSE,
+    'cachedir' => '',
+    'char_set' => 'utf8',
+    'dbcollat' => 'utf8_general_ci',
+    'swap_pre' => '',
+    'encrypt' => FALSE,
+    'compress' => FALSE,
+    'stricton' => FALSE,
+    'failover' => array(),
+    'save_queries' => TRUE
 );
