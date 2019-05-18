e3c69c5afb8aa1168d232d0f8928d55cd627aec1
DRAGON <carlos5t@hotmail.com>
2019-05-18 11:52:09 -0500

    404 modify

--- application/config/routes.php
+++ application/config/routes.php
@@ -50,4 +50,5 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |		my-controller/my-method	-> my_controller/my_method
 */
 $route['default_controller'] = 'Login';
+$route['404_override'] = 'MyCustom404Ctrl';
 $route['translate_uri_dashes'] = FALSE;
