9a55a8cc547c99475994e8d5a313a9e8d6f6f500
DRAGON <carlos5t@hotmail.com>
2019-07-30 11:08:12 -0500

    formatting grupo model

--- application/models/adm/grupo_model.php
+++ application/models/adm/grupo_model.php
@@ -1,29 +1,29 @@
 <?php
 
-class Grupo_model extends CI_Model{
-    
+class Grupo_model extends CI_Model
+{
+
     private $id_grupo;
     private $nombre_grupo;
     private $icon_grupo;
     private $orden_grupo;
     private $estado;
-    
-    public function __construct() {
+
+    public function __construct()
+    {
         parent::__construct();
-        
     }
-    
-    public function obtener_grupos($id_usuario){
-        //test
+
+    public function obtener_grupos($id_usuario)
+    {
+
         $this->db->distinct();
         $this->db->select('G.id_grupo,nombre_grupo,icon_grupo');
-        $this->db->join('usuario_has_formulario_grupo as UFG','G.id_grupo = UFG.id_grupo');
-       $this->db->order_by("orden_grupo", "ASC");
-        $this->db->group_by("G.id_grupo,nombre_grupo,icon_grupo"); 
-        $res_grupos = $this->db->get_where('grupo G',array('UFG.id_usuario' => $id_usuario,'G.estado' => 'Y'));
-//        return $this->db->last_query();
+        $this->db->join('usuario_has_formulario_grupo as UFG', 'G.id_grupo = UFG.id_grupo');
+        $this->db->order_by("orden_grupo", "ASC");
+        $this->db->group_by("G.id_grupo,nombre_grupo,icon_grupo");
+        $res_grupos = $this->db->get_where('grupo G', array('UFG.id_usuario' => $id_usuario, 'G.estado' => 'Y'));
+        //        return $this->db->last_query();
         return $res_grupos;
-        
     }
-    
 }
